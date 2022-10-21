e16a286d6b9db186524f1af597e6e70171cb8a61
Trevor Smith <trevormax.smith@gmail.com>
2022-10-21 18:41:56 -0400

    formatting

--- project_2/card.h
+++ project_2/card.h
@@ -4,23 +4,22 @@
 // header file for card class
 using namespace std;
 
-class card 
-{
-	public:
-		card(int suit, int value);
-		card(const card &other);
-		void setValue(int value);
-		void setSuit(int suit);
-		string getValue();
-		string getSuit();
-		bool getFlipped();
-		friend void operator<<(ostream &ostr, card rhs);
-		card operator=(const card &other);
-		void flip();
-		int getIntegerValue();
+class card {
+public:
+  card(int suit, int value);
+  card(const card &other);
+  void setValue(int value);
+  void setSuit(int suit);
+  string getValue();
+  string getSuit();
+  bool getFlipped();
+  friend void operator<<(ostream &ostr, card rhs);
+  card operator=(const card &other);
+  void flip();
+  int getIntegerValue();
 
-	private:
-		string cardSuit, cardValue;
-		int integerValue;
-		bool isFlipped;
+private:
+  string cardSuit, cardValue;
+  int integerValue;
+  bool isFlipped;
 };
--- project_2/main.cpp
+++ project_2/main.cpp
@@ -5,95 +5,80 @@
 using namespace std;
 
 int updateScore(int score, card chosenCard) {
-    //global function to update a user's score based on the card they selected
-    int cardValue = chosenCard.getIntegerValue();
+  // global function to update a user's score based on the card they selected
+  int cardValue = chosenCard.getIntegerValue();
 
-    if (cardValue == 1) 
-    {
-        score += 10;
-    } else if (cardValue >= 11 && cardValue <= 13) 
-    {
-        score += 5;
-    } else if (cardValue >= 8 && cardValue <= 10) 
-    {
-        score += 0;
-    } else if (cardValue == 7) 
-    {
-        score /= 2;
-    } else if (cardValue >= 2 && cardValue <= 6) 
-    {
-        score = 0;
-    }
+  if (cardValue == 1) {
+    score += 10;
+  } else if (cardValue >= 11 && cardValue <= 13) {
+    score += 5;
+  } else if (cardValue >= 8 && cardValue <= 10) {
+    score += 0;
+  } else if (cardValue == 7) {
+    score /= 2;
+  } else if (cardValue >= 2 && cardValue <= 6) {
+    score = 0;
+  }
 
-    if (chosenCard.getSuit() == "Heart") 
-    {
-        score++;
-    }
-    return score;
+  if (chosenCard.getSuit() == "Heart") {
+    score++;
+  }
+  return score;
 }
 
 void playFlip() {
-    // runs the main function that creates a deck and prints it,
-    // and then shuffles before reprinting to confirm changes
+  // runs the main function that creates a deck and prints it,
+  // and then shuffles before reprinting to confirm changes
 
-    deck my_deck = deck();
+  deck my_deck = deck();
 
-    int response = -1;
-    int score = 0;
+  int response = -1;
+  int score = 0;
 
-    for (int i = 0; i < 3; i++) 
-    {
-        my_deck.shuffle();
+  for (int i = 0; i < 3; i++) {
+    my_deck.shuffle();
+  }
+
+  deck gameDeck = deck(my_deck.deal());
+
+  for (int i = 0; i < 23; i++) {
+    gameDeck.replace(my_deck.deal());
+  }
+  cout << "Here are the remaining cards\n";
+  cout << my_deck;
+  cout << "Here are the cards in your hand\n";
+  cout << gameDeck;
+
+  while (1) {
+    cout << "Choose a card from 1 to 24, or 0 to end the game." << endl;
+    cin >> response;
+    while (response < 0 || response > 24) {
+      cout << "The card you selected does not exist. Please choose a card from "
+              "1 to 24, or 0 to end the game"
+           << endl;
+      cin >> response;
     }
 
-    deck gameDeck = deck(my_deck.deal());
-
-    for (int i = 0; i < 23; i++) 
-    {
-        gameDeck.replace(my_deck.deal());
+    if (response == 0) {
+      cout << "Your final score is: " << score << endl;
+      break;
+    } else {
+      node<card> *chosenCard = gameDeck.goToCard(response);
+      cout << "Here are the cards in your hand\n";
+      cout << gameDeck;
+      if (chosenCard->nodeValue.getFlipped()) {
+        cout << "You already chose that card before, cheater!!!!\n";
+      } else {
+        chosenCard->nodeValue.flip();
+        cout << chosenCard->nodeValue;
+        score = updateScore(score, chosenCard->nodeValue);
+        cout << "Your new score is " << score << endl;
+      }
     }
-    cout << "Here are the remaining cards\n";
-    cout << my_deck;
-    cout << "Here are the cards in your hand\n";
-    cout << gameDeck;
-
-    while (1)
-    {
-        cout << "Choose a card from 1 to 24, or 0 to end the game." << endl;
-        cin >> response;
-        while (response < 0 || response > 24)
-        {
-            cout << "The card you selected does not exist. Please choose a card from 1 to 24, or 0 to end the game" << endl;
-            cin >> response;
-        }
-
-        if (response == 0) 
-        {
-            cout << "Your final score is: " << score << endl;
-            break;
-        }
-        else 
-        {
-            node<card>* chosenCard = gameDeck.goToCard(response);
-            cout << "Here are the cards in your hand\n";
-            cout << gameDeck;
-            if (chosenCard->nodeValue.getFlipped()) 
-            {
-                cout << "You already chose that card before, cheater!!!!\n";
-            }
-            else 
-            {
-                chosenCard->nodeValue.flip();
-                cout << chosenCard->nodeValue;
-                score = updateScore(score, chosenCard->nodeValue);
-                cout << "Your new score is " << score << endl;
-            }
-        }
-    }
+  }
 }
 
-int main() 
-{
-    playFlip();
-    return 0;
+int main() {
+  playFlip();
+  return 0;
 }
