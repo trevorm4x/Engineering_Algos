a7dc944de327f2b82210c289b27b9faa9bab2202
Trevor Smith <trevormax.smith@gmail.com>
2022-12-04 17:23:26 -0500

    add stuff in main with the Z thing

--- Project_4/main.cpp
+++ Project_4/main.cpp
@@ -2,6 +2,49 @@
 
 using namespace std;
 
+
+int readBigFile(string fileName)
+// takes in the filename of the big puzzles file and reads in each line
+// to a new board and solves it, storing the number of recursive calls
+{
+  int total_calls = 0;
+  int num_puzzles = 0;
+  fstream myFile;
+  myFile.open(fileName.c_str(), ios::in);
+  int rows, cols;
+  // z character for finding end of file
+  char Z = 90;
+  if (myFile.is_open()) // ensures that the file is opened properly before
+                        // reading in words
+  {
+    bool file_read = false;
+    // loop on each line of the file and make a board with each until we get
+    // the one that ends with a "z"
+    while (!file_read) {
+      // 81-length string
+      string puzzle;
+      // this probably doesn't work but not sure how to ATM
+      myFile >> puzzle;
+      ++num_puzzles;
+      if (puzzle[puzzle.length() - 1] == Z) {
+        // remove the "Z"
+        puzzle.pop_back();
+        file_read = true;
+      }
+      board newboard;
+      newboard.readFromString(puzzle);
+      int num_calls = newboard.solve();
+      if (num_calls == -1) {
+        cout << "FAILED!\n";
+      }
+      else {
+        total_calls += num_calls;
+      }
+    }
+  }
+  return total_calls / num_puzzles;
+}
+ 
 // main function that initializes a game board and executes methods for testing
 int main() {
   board board1;
